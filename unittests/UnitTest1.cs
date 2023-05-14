83a22780db14f3225f44695b269b82bbb766b746
Marcel de Vries <vriesmarcel@hotmail.com>
2023-05-14 14:07:39 +0200

    add unit tests

---
+++ unittests/UnitTest1.cs
@@ -0,0 +1,27 @@
+namespace unittests
+{
+    [TestClass]
+    public class Fast_Unit_Tests
+    {
+        [TestMethod("validate numbers are > 0")]
+        public void numbers_are_greater_then_0()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod("validate numbers are < 10")]
+        public void numbers_are_greater_smaller_than_10()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod("validate numbers are odd")]
+        public void numbers_are_odd()
+        {
+            Assert.IsTrue(false);
+        }
+        [TestMethod("validate numbers are even")]
+        public void numbers_are_even()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
